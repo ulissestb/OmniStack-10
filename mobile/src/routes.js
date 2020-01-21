d1a44ea90b401cd36eba634e0fd754c464e97965
Ulisses Nascimento <ulisses.melo.nascimento@gmail.com>
2020-01-21 15:26:52 -0300

    Adicionando mapa

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,13 +2,30 @@ import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 
 import Main from './pages/Main'
-import Profile from './pages/profile'
+import Profile from './pages/Profile'
 
-const Routes = createAppContainer({
-  createStackNavigator({
-    Main,
-    Profile
-  })
+const navigator = createStackNavigator({
+  Main: {
+    screen: Main,
+    navigationOptions: {
+      title: 'DevRadar'
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      title: 'Perfil no Github'
+    }
+  },
+}, {
+  defaultNavigationOptions: {
+    headerTintColor: '#FFF',
+    headerStyle: {
+      backgroundColor: '#7D40E7'
+    }
+  }
 })
+
+const Routes = createAppContainer(navigator)
 
 export default Routes
